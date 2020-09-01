1a6127c79da047b7bc6c9d1806f1211caee01fb5
Andrey Lozovoy <lozovoya@gmail.com>
2020-09-01 15:45:15 +0300

    it works!!

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,60 @@
 package main
 
+import (
+	"context"
+	"github.com/lozovoya/gohomework10_2/pkg/qr"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+const line = "www.yandex.ru"
+const filename = "yandex.png"
+
 func main() {
 
+	qrURL, ok := os.LookupEnv("QRURL")
+	if !ok {
+		log.Println("No URL available")
+		os.Exit(1)
+	}
+	qrVersion, ok := os.LookupEnv("QRVERSION")
+	if !ok {
+		log.Println("No version available")
+		os.Exit(1)
+	}
+	qrTimeout, ok := os.LookupEnv("QRTIMEOUT")
+	if !ok {
+		log.Println("No timeout available")
+		os.Exit(1)
+	}
+	timeout, err := strconv.Atoi(qrTimeout)
+	if err != nil {
+		log.Println("wrong timeout format")
+		os.Exit(1)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
+	svc := qr.NewService(
+		qrURL,
+		qrVersion,
+		ctx,
+		&http.Client{},
+	)
+	data, err := svc.Encode(line)
+
+	file, err := os.Create("test.png")
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+	err = ioutil.WriteFile("test.png", data, 0777)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
 }
---
+++ pkg/qr/qr.go
@@ -0,0 +1,80 @@
+package qr
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+const (
+	apiMethod         = "create-qr-code"
+	headerContentType = "Content-Type"
+	formURLEncoded    = "application/x-www-form-urlencoded"
+)
+
+type Service struct {
+	url     string
+	version string
+	ctx     context.Context
+	client  *http.Client
+}
+
+func NewService(
+	url string,
+	version string,
+	ctx context.Context,
+	client *http.Client,
+) *Service {
+	return &Service{
+		url: url, version: version, ctx: ctx, client: client,
+	}
+}
+
+func (s *Service) Encode(line string) (data []byte, err error) {
+	values := make(url.Values)
+	reqURL := fmt.Sprintf("%s/%s/%s", s.url, s.version, apiMethod)
+	values.Set("data", line)
+	values.Set("size", "100x100")
+
+	req, err := http.NewRequestWithContext(
+		s.ctx,
+		http.MethodGet,
+		fmt.Sprintf("%s?%s", reqURL, values.Encode()),
+		nil,
+	)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	req.Header.Set(headerContentType, formURLEncoded)
+
+	resp, err := s.client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	//resp, err := http.Get(fmt.Sprintf("%s?%s",reqURL, values.Encode()))
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(respBody))
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	defer func() {
+		if cerr := resp.Body.Close(); cerr != nil {
+			if err == nil {
+				log.Println(err)
+				err = cerr
+			}
+		}
+	}()
+
+	return respBody, nil
+}
